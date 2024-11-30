bb40bc7258ebdc36354b66ad54bad95cdfed58c6
zheer barzan <zheerbarzan85@outlook.com>
2024-11-30 23:25:52 +0300

    adding some components and the settings page

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:readiculous/views/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              //logo
+              DrawerHeader(
+                child: Icon(Icons.message,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    size: 50),
+              ),
+              //home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              //settings list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SettingsPage()));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
